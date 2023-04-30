83ec9d232204a5e8167fdefbd830c31f51c178f8
Joe Letner <jrletner@gmail.com>
2023-04-30 15:00:30 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  items = ['Coach Will', 'David', 'Victoria', 'Jon', 'Sara', 'Eric', 'Joe'];
   title = 'DragDrop';
+  ngOnInit(): void {}
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
